2df5df00cf5b2516d3f6ccc6067daf401cb61ba8
sheldon <sheldon.jf.huang@pccw.com>
2022-12-22 10:20:42 +0800

    init Koa2

---
+++ koa.mjs
@@ -0,0 +1,26 @@
+import Koa from "koa";
+const app = new Koa();
+
+// app.use(async (ctx) => {
+//   ctx.body = "Hello World";
+// });
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set("X-Response-Time", `${ms}ms`);
+});
+
+// logger
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+app.use(async (ctx) => {
+  ctx.body = "Hello World";
+});
+app.listen(3000);
